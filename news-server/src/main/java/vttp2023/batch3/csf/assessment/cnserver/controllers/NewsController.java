36e8c2a39809679246818d61afee4746cbbb1472
russell258 <duckie258@gmail.com>
2023-08-31 14:56:28 +0800

    backend task 1 controller

--- news-server/src/main/java/vttp2023/batch3/csf/assessment/cnserver/controllers/NewsController.java
+++ news-server/src/main/java/vttp2023/batch3/csf/assessment/cnserver/controllers/NewsController.java
@@ -1,12 +1,37 @@
 package vttp2023.batch3.csf.assessment.cnserver.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.json.JsonString;
+import vttp2023.batch3.csf.assessment.cnserver.repositories.ImageRepository;
+import vttp2023.batch3.csf.assessment.cnserver.repositories.NewsRepository;
 
 @RestController
+@RequestMapping()
 public class NewsController {
 
-	// TODO: Task 1
+	@Autowired
+	NewsRepository newsRepo;
 
+	@Autowired
+	ImageRepository imageRepo;
+
+	// TODO: Task 1
+	@PostMapping(path="/upload", consumes = MediaType.MULTIPART_FORM_DATA)
+	public ResponseEntity<String> postUpload(@RequestPart String title, @RequestPart MultipartFile file, 
+							@RequestPart String description, @RequestPart JsonString tags ){
+		String newsid = imageRepo.saveImage(file);
+		newsRepo.toInsert(newsid, title, description, imageurl, tags);
+
+		//return object id
+	}
 
 	// TODO: Task 2
 
--- news-server/src/main/java/vttp2023/batch3/csf/assessment/cnserver/repositories/NewsRepository.java
+++ news-server/src/main/java/vttp2023/batch3/csf/assessment/cnserver/repositories/NewsRepository.java
@@ -1,13 +1,41 @@
 package vttp2023.batch3.csf.assessment.cnserver.repositories;
 
+
+import java.io.File;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
+
+import jakarta.json.JsonString;
 
 @Repository
 public class NewsRepository {
 
+	@Autowired
+	private MongoTemplate mongo;
+
 	// TODO: Task 1 
+	// native query: 
+	// db.tags.insert({
+	// 	<name> : <data>
+	// })
 	// Write the native Mongo query in the comment above the method
-	
+	public String toInsert (String newsid, String title, String description, String imageurl, JsonString splitTags){
+		Document form = new Document();
+		form.put("_id", newsid);
+		form.put("title",title);
+		form.put("postDate", System.currentTimeMillis());
+		form.put("description",description);
+		form.put("image", imageurl);
+		form.put("tags", splitTags);
+
+		Document newDoc = mongo.insert(form, "tags");
+		String id = newDoc.getObjectId(newDoc).toString();
+		return id;
+	}
 
 	// TODO: Task 2 
 	// Write the native Mongo query in the comment above the method
